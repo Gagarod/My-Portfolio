8ee8ec9a4ba113fb40e79e9296bb217b7fe8d1e5
singhsduos <ns9628491678@gmail.com>
2021-11-04 08:41:47 +0530

    projectSectionComplete

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-const projectSection = document.querySelector('#projects');
+const allProjects = document.getElementById('allProject');
 
 
+// fetch project start
+function getProjects() {
+    return fetch("project.json")
+        .then(response => response.json())
+        .then(data => {
+            return data
+        })
+}
+
+
+function showProjects(project) {
+    let projectsHTML = "";
+    project.forEach(projects => {
+        projectsHTML += `
+        <div class="card" >
+                <img class="projectImg" src="${projects.image}" alt="">
+                    <div class="titleAndIconDiv">
+                    <h3 class="projectTitle">${projects.name}</h3>
+                        <div class="projectIcons">
+                        <a class="projectLinks" href="${projects.links.code}" target="_blank"
+                                rel="noopener noreferrer">
+                                <i class="fab fa-github fa-lg"></i>
+                            </a>
+                        <a class="projectLinks" href="${projects.links.view}" target="_blank"
+                                rel="noopener noreferrer">
+                                <i class="fas fa-eye"></i>
+                            </a>
+                        </div>
+                    </div>
+              </div>`
+        allProjects.innerHTML = projectsHTML;
+
+    });
+
+
+}
+
+
+getProjects().then(data => {
+    showProjects(data);
+});
