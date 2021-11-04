4952fafb7a696f3f045e8d3cacbbe9e0421159ed
singhsduos <ns9628491678@gmail.com>
2021-11-04 19:29:41 +0530

    vieMore btn created

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const navIcon = document.querySelectorAll('.navLink i');
 const scrollDownBtn = document.querySelector('.scrollDownBtn');
 const lightModeBtn = document.getElementById('lightMode');
 const darkModeBtn = document.getElementById('darkMode');
-
+const showBtnDiv = document.querySelector('.showBtnDiv');
+const showAllBtn = document.getElementById('showAll');
 
 scrollDownBtn.addEventListener('click', function () {
     window.scrollTo(0, document.querySelector("body").scrollHeight);
@@ -22,32 +23,46 @@ function getProjects() {
         });
 }
 
+var state = "less";
+let projectsHTML = "";
+
+showAllBtn.addEventListener('click', function () {
+    showBtnDiv.style.display = 'none';
+    state = 'more';
+});
 
 function showProjects(project) {
-    let projectsHTML = "";
-    project.forEach(projects => {
-        projectsHTML += `
+    if (state == 'less') {
+        for (let i = 0; i < 9; i++) {
+            projectCreation(project[i]);
+        }
+    }
+    else if (state == 'more') {
+        for (let i = 0; i < project.length; i++) {
+            projectCreation(project[i]);
+        }
+    }
+}
+
+function projectCreation(project) {
+    projectsHTML += `
         <div class="card" >
-                <img class="projectImg" src="${projects.image}" alt="">
+                <img class="projectImg" src="${project.image}" alt="">
                     <div class="titleAndIconDiv">
-                    <h3 class="projectTitle">${projects.name}</h3>
+                    <h3 class="projectTitle">${project.name}</h3>
                         <div class="projectIcons">
-                        <a class="projectLinks" href="${projects.links.code}" target="_blank"
+                        <a class="projectLinks" href="${project.links.code}" target="_blank"
                                 rel="noopener noreferrer">
                                 <i class="fab fa-github fa-lg"></i>
                             </a>
-                        <a class="projectLinks" href="${projects.links.view}" target="_blank"
+                        <a class="projectLinks" href="${project.links.view}" target="_blank"
                                 rel="noopener noreferrer">
                                 <i class="fas fa-eye"></i>
                             </a>
                         </div>
                     </div>
               </div>`
-        allProjects.innerHTML = projectsHTML;
-
-    });
-
-
+    allProjects.innerHTML = projectsHTML;
 }
 
 
